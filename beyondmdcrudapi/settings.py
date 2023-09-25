2fe0dde06c01be6cc4e67c90f65dadb1c085d70e
mzsellem <marleysellem@gmail.com>
2023-09-25 15:24:52 -0400

    fix type in cors middleware

--- beyondmdcrudapi/settings.py
+++ beyondmdcrudapi/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'coresheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
