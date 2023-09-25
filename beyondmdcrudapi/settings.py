b937f87fb922af7194c45007ae29aba545b56b8a
mzsellem <marleysellem@gmail.com>
2023-09-25 14:41:23 -0400

    make migrations

--- beyondmdcrudapi/settings.py
+++ beyondmdcrudapi/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permission.AllowAny']}
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']}
 
 ROOT_URLCONF = 'beyondmdcrudapi.urls'
 
--- beyondmdcrudapi/urls.py
+++ beyondmdcrudapi/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url 
+from django.urls import re_path as url 
 from app.views import *
 
 urlpatterns = [
